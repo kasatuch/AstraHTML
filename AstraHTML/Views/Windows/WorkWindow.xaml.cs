9168958e1d7e6962fc0b136d461d8cd795dd819e
dimainnature <dimainnature@yandex.com>
2021-04-02 06:39:36 +0400

    Сделал словарь ресурсов, состоящий из классной темы для кнопок, локальным

--- AstraHTML/Views/Windows/WorkWindow.xaml.cs
+++ AstraHTML/Views/Windows/WorkWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,27 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AstraHTML
+namespace AstraHTML.Views.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для WorkWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WorkWindow : Window
     {
-        public MainWindow()
+        public WorkWindow()
         {
             InitializeComponent();
-        }
-
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if(e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
         }
     }
 }
