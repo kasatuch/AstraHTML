6e6a5d66a2c3cc686fca2d9ce671b051878ff48e
dimainnature <dimainnature@yandex.com>
2021-04-07 21:54:49 +0400

    Добавление метода, который запрещает вводить любые символы, кроме цифр в поле для целых чисел

--- AstraHTML/Views/OthWindows/AddNewStaffWindow.xaml.cs
+++ AstraHTML/Views/OthWindows/AddNewStaffWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,6 +22,12 @@ namespace AstraHTML.Views.OthWindows
         {
             InitializeComponent();
             DataContext = new DataManageVM();
+        }
+
+        private void PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
